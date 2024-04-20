8571eccbd5f492819125a9901cd30befc89ebbe0
Capitalkhan <skprogramminghi@gmail.com>
2024-04-20 13:53:42 +0500

    adding post api project

--- get/lib/main.dart
+++ get/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_api/screen/post_apis_view.dart';
 import './screen/user_api_view.dart';
+import './screen/fetch_api_without_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      home: UserApiView(),
+      home: FetchApiWithoutModel(),
     );
   }
 }
--- get/lib/screen/fetch_api_without_model.dart
+++ get/lib/screen/fetch_api_without_model.dart
@@ -21,14 +21,50 @@ class FetchApiWithoutModel extends StatelessWidget {
       }
     }
     
-    return FutureBuilder(future: getApi(),
-        builder: (context,snapShot){
-          if(snapShot.connectionState == ConnectionState.waiting){
-            return CircularProgressIndicator();
-          }else{
-            return "${data[0]["name"]}".text.make();
+    return Scaffold(
+
+      body: FutureBuilder(future: getApi(),
+          builder: (context,snapShot){
+            if(snapShot.connectionState == ConnectionState.waiting){
+              return CircularProgressIndicator().centered();
+            }else{
+              return ListView.builder(itemCount: data.length,itemBuilder: (context,index) {
+                return Card(
+                  color: Colors.blue,
+                  child: Column(
+                    children: [
+                      ReuseableRow(title:"Name" , value: data[index]['name'],),
+                      ReuseableRow(title:"Email" , value: data[index]['email'],),
+                      ReuseableRow(title:"City" , value: data[index]['address']['city'],),
+                      ReuseableRow(title:"Company Name" , value: data[index]['company']['name'],),
+                    ],
+                  ),
+                );
+              });
+
+            }
           }
-        }
+      )
+    );
+  }
+}
+class ReuseableRow extends StatelessWidget {
+  final String title;
+  final String value;
+
+  ReuseableRow({required this.title,required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "$title".text.make(),
+          "$value".text.make(),
+        ],
+      ),
     );
   }
 }
